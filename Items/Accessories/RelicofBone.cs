e1646738542b91e7a307c90832f7fa4bd6ec977c
KrypticKralo <70592788+KrypticKralo@users.noreply.github.com>
2021-07-09 04:06:02 -0400

    Created two accessories
    If Relic of Bone is made incorrectly, please let me know. Relic of Ice's effect is currently beyond me.

---
+++ Items/Accessories/RelicofBone.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ElysiumMod.Items.Accessories
+{
+    public class RelicofBone : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Relic of Bone");
+            Tooltip.SetDefault("Ancient Artifact of skeletons n/Used to create the 'Ultimate Relic'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.accessory = true;
+
+            //Temporary until a sprite is designed
+            item.height = 20;
+            item.width = 20;
+            //====================================
+
+            item.rare = ItemRarityID.LightRed;
+            item.value = Item.sellPrice(gold: 1);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            /*I THINK this is correct, but please alert me if I'm wrong
+                                                        -Kryptic Kralo*/
+            player.statDefense += 4;
+        }
+    }
+}
---
+++ Items/Accessories/RelicofIce.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ElysiumMod.Items.Accessories
+{
+    public class RelicofIce : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Relic of Ice");
+            Tooltip.SetDefault("Unmeltable substance n/Used to create the 'Ultimate Relic'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.accessory = true;
+
+            //Temporary until sprite is made
+            item.width = 20;
+            item.height = 20;
+            //==============================
+
+            item.rare = ItemRarityID.LightRed;
+            item.value = Item.sellPrice(gold: 1);
+            
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+           //On critical hit with any weapon, inflic Frostburn on opponent
+        }
+    }
+}
